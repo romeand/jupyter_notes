3d9b7c5b75ed42b7259c486a35567cd68acc612b
romeand <28819880+romeand@users.noreply.github.com>
2024-12-12 13:20:59 -0600

    Added to a repo the practice py and ipynb practice files

---
+++ cpus.py
@@ -0,0 +1,4 @@
+import os # importar la librería os
+
+cpu_num = os.cpu_count() # obtener el número de CPU en el sistema
+print(f"Mi computadora tiene {cpu_num} núcleos de CPU") # imprimir el número de CPU
